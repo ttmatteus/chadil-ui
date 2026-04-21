c751d4df02529d1ef2168b0cdb0ed692321c9923
zabeluna <personalife503@gmail.com>
2026-04-20 21:04:43 -0300

    feat: add header to landing

--- src/modules/landing/page.tsx
+++ src/modules/landing/page.tsx
@@ -1,6 +1,9 @@
+import { Header } from "@/shared/components/header";
+
 export function LandingPage() {
   return (
     <main className="min-h-screen">
+      <Header />
       <h1 className="text-4xl font-bold text-center py-20">
         chadil.ui
       </h1>
